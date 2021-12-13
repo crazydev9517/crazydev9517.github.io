bd674ce93f4db52be5943bd825775808420e0b26
Robinson Stewart <94480152+robpark04@users.noreply.github.com>
2021-12-13 20:33:03 +0200

    first

---
+++ static/js/22.0c8fa17b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[22],{1018:function(a,s,d){}}]);
+//# sourceMappingURL=22.0c8fa17b.chunk.js.map
